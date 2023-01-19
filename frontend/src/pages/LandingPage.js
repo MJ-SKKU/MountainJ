ffc42e28281d48cb63b3729afb12e26171418999
seongwon <lovesomedino@gmail.com>
2023-01-20 02:12:30 +0900

    refactor: 카카오 로그인 API 리팩토링(중간 화면 추가)

--- frontend/src/pages/LandingPage.js
+++ frontend/src/pages/LandingPage.js
@@ -1,43 +1,9 @@
 import React, { Fragment } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Outlet } from "react-router-dom";
 import KakaoLogInImage from "../assets/images/kakao_login.png";
-import { API, CLIENT_URL } from "../config";
+import { API } from "../config";
 
 const LandingPage = () => {
-
-  const href = window.location.href;
-  let params = new URL(window.location.href).searchParams;
-  let code = params.get("code");
-
-  if (code!=null){
-        const formData = new FormData();
-        formData.append("code", code);
-
-        axios.post(`${API.LOGIN}`, formData).then((res) => {
-
-          code = null;
-
-          const user_id = res['data']['user']['id'];
-          const userObject = res['data']['user'];
-          navigate("/user", { state: { userId: user_id, userObject:userObject } });
-
-        });
-  }
-  else{
-    code = null;
-  }
-
-  const navigate = useNavigate();
-  const handleKakaoLogInClick = async (e) => {
-    e.preventDefault();
-    window.location.href = `${API.KAKAO}`;
-  };
-
-  const handleNonMemberClick = () => {
-    alert("todo: 비회원 정산");
-  };
-
   return (
     <Fragment>
       <div className="fixed inset-0 w-screen h-screen bg-lime -z-10" />
@@ -52,8 +18,17 @@ const LandingPage = () => {
         </div>
         <Outlet />
         <div>
-          <img className="w-full mb-3" src={KakaoLogInImage} alt="kakao_login" onClick={handleKakaoLogInClick} />
-          <button className="w-full rounded-md bg-white" style={{ aspectRatio: "20/3" }} type="button" onClick={handleNonMemberClick}>
+          <a href={API.KAKAO}>
+            <img className="w-full mb-3" src={KakaoLogInImage} alt="kakao_login" />
+          </a>
+          <button
+            className="w-full rounded-md bg-white"
+            style={{ aspectRatio: "20/3" }}
+            type="button"
+            onClick={() => {
+              alert("todo: 비회원 정산");
+            }}
+          >
             비회원으로
           </button>
         </div>
