2208242257537c34ac07e6cbed1703d987896661
seongwon <lovesomedino@gmail.com>
2023-01-20 03:08:48 +0900

    feat: 카카오 로그인 API redirect uri 변경 및 라우팅

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,7 +5,7 @@ const HOST_URL_SERVER = "http://ec2-43-201-71-106.ap-northeast-2.compute.amazona
 const SERVER_URL = HOST_URL_SERVER + ":8000/";
 const CLIENT_URL = HOST_URL + ":3000/";
 
-const REDIRECT_URI = CLIENT_URL;
+const REDIRECT_URI = CLIENT_URL + "/kakao/login";
 const REST_API_KEY = "43f9c4625042bd2d0d174ecf3708b12e";
 
 const BASE_URL = SERVER_URL + "api";
