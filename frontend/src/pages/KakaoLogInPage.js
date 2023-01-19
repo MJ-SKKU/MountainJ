2ac95e8c38cb841daa88de6a1167d26d052eab56
seongwon <lovesomedino@gmail.com>
2023-01-20 03:10:19 +0900

    refactor: 카카오 로그인 API 구조 변경(중간 페이지 추가)

---
+++ frontend/src/pages/KakaoLogInPage.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { API } from "../config";
+
+const KakaoLogInPage = () => {
+  const navigate = useNavigate();
+
+  const params = new URL(window.location.href).searchParams;
+  const authCode = params.get("code");
+
+  const authCodeformData = new FormData();
+  authCodeformData.append("code", authCode);
+
+  axios.post(`${API.LOGIN}`, authCodeformData).then((res) => {
+    console.log(res);
+    const userInfo = res.data.user;
+    navigate("/user", { state: { userInfo: userInfo } });
+  });
+
+  return <div>카카오 로그인중입니다...</div>;
+};
+
+export default KakaoLogInPage;
