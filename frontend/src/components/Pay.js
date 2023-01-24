342d210ab2f125098e9eceefdb7244422cbf2876
jaewon <tsfo1489@gmail.com>
2023-01-24 16:45:39 +0900

    feat: 결제내역 삭제 버튼 생성

--- frontend/src/components/Pay.js
+++ frontend/src/components/Pay.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { FiChevronDown } from "react-icons/fi";
+import { FiChevronDown, FiTrash } from "react-icons/fi";
 import UserProfile from "./UserProfile";
 import axios from "axios";
 import {API} from "../config";
@@ -11,10 +11,15 @@ const Pay = ({ members, payer_id, money, title, pay_id }) => {
         axios.get(`${API.PAYMEMBERS}/${pay_id}`).then((res) => setPayMembers([...res.data]));
     }, [])
 
-
-    const MemberListDropDownIconClick = () => {
-        alert("todo: 결제 내역 참여자 드롭다운");
+    const PayListAccordionIconClick = () => {
+        alert("todo: 결제 내역 참여자 아코디언");
     };
+
+    const PayDeleteClick = () => {
+        alert("todo: 결제 내역 삭제");
+        axios.delete();
+    }
+
     let username;
 
     for (let member of members){
@@ -23,31 +28,34 @@ const Pay = ({ members, payer_id, money, title, pay_id }) => {
         }
     }
   return (
-    <div className="d-flex flex-column mb-3">
-        <div className="flex justify-between mx-auto items-center w-11/12 pt-3 px-5 pb-2.5 border-none rounded-md bg-white shadow">
+    <div className="flex flex-col mb-3">
+        <div className="flex justify-between mx-auto items-center w-11/12 pt-3 px-5 pb-2.5 border-none rounded-md bg-white shadow z-40">
           <UserProfile username={username} />
           <div className="flex flex-col justify-evenly items-center">
             <span className="text-lg font-semibold">{money}원</span>
             <span>{title}</span>
           </div>
-          <FiChevronDown size="24" onClick={MemberListDropDownIconClick} />
+        <FiChevronDown size="24" onClick={PayListAccordionIconClick} />
         </div>
-        <div className="flex justify-between mx-auto items-center w-11/12 pb-2.5 border-none bg-none">
-            <div className="flex justify-between mx-auto items-center w-11/12 pt-3 pl-5 pb-2.5 pr-3 border bg-white shadow overflow-x-auto">
-                <div className="flex pr-3">
-                    참여자
+        <div className="flex justify-between mx-auto items-center w-11/12 pb-2.5 border-none bg-none z-30">
+            <div className="flex flex-col w-full border bg-white shadow">
+                <div className="flex justify-between mx-auto items-center w-full -m-1 pt-4 pl-5 pb-2.5 pr-3 overflow-x-auto">
+                    <div className="flex pr-3">
+                        참여자
+                    </div>
+                    {paymembers.map((member) => {
+                        return (
+                            <div key={member.member_id} className="flex">
+                                <UserProfile username={member.username}/>
+                                <div className="mr-5"/>
+                            </div>
+                        );
+                    })
+                    }
                 </div>
-                {paymembers.map((member) => {
-                    // console.log('.');
-                    // console.log(member);
-                    return (
-                        <div key={member.member_id} className="flex">
-                            <UserProfile username={member.username}/>
-                            <div className="mr-5"/>
-                        </div>
-                    );
-                })
-                }
+                <div className="mx-auto mb-2">
+                    <FiTrash size="12" onClick={PayDeleteClick} />
+                </div>
             </div>
         </div>
     </div>
