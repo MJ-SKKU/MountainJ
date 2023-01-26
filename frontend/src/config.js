4cef1844d13a719c6172a4e496f249018a87a55c
jaewon <tsfo1489@gmail.com>
2023-01-26 22:05:27 +0900

    fix: HOST URL 긴급 수정

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-// const HOST_URL = "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
-const HOST_URL = "http://localhost";
+const HOST_URL = "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
+// const HOST_URL = "http://localhost";
 
 const HOST_URL_SERVER = "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
 // const SERVER_URL = HOST_URL_SERVER + ":8000/";
