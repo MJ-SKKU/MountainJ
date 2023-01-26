0037d0c358c5454a40642f935f520c0f3d408565
Suhyeon <fbtngus2789@naver.com>
2023-01-27 01:02:42 +0900

    refactor: prettier 컨벤션에 따른 구조로 코드 리팩토링

--- frontend/src/components/Modal.js
+++ frontend/src/components/Modal.js
@@ -9,8 +9,14 @@ const Modal = ({ setIsModalOpen }) => {
 
   return (
     <div className="flex flex-col justify-center items-center fixed inset-0 z-20">
-      <div className="absolute inset-0" style={{ background: "rgba(11, 19, 30, 0.37)" }} />
-      <div className="flex flex-col w-11/12 p-4 rounded-md bg-white z-10" style={{ maxWidth: "360px", minHeight: "420px" }}>
+      <div
+        className="absolute inset-0"
+        style={{ background: "rgba(11, 19, 30, 0.37)" }}
+      />
+      <div
+        className="flex flex-col w-11/12 p-4 rounded-md bg-white z-10"
+        style={{ maxWidth: "360px", minHeight: "420px" }}
+      >
         <div className="flex justify-end">
           <IoCloseOutline size="24" onClick={handleCloseIconClick} />
         </div>
