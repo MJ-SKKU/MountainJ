8cb12b5f118e261c1be1752406f7152db281bb85
Seran Kim <49436143+theran23@users.noreply.github.com>
2023-01-28 16:48:35 +0900

    fix: config 수정

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,11 @@
-// const HOST_URL =
-//   "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
-const HOST_URL = "http://localhost";
+const HOST_URL =
+  "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
+// const HOST_URL = "http://localhost";
 
 const HOST_URL_SERVER =
   "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
-// const SERVER_URL = HOST_URL_SERVER + ":8000/";
-const SERVER_URL = HOST_URL + ":8000/";
+const SERVER_URL = HOST_URL_SERVER + ":8000/";
+// const SERVER_URL = HOST_URL + ":8000/";
 const CLIENT_URL = HOST_URL + ":3000/";
 
 const REDIRECT_URI = CLIENT_URL + "/kakao/login";
