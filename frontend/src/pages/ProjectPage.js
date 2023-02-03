0042685e252d5446e16c35b1b9693e6de1123c6c
Seran Kim <49436143+theran23@users.noreply.github.com>
2023-02-04 02:12:37 +0900

    fix: memebers 오타

--- frontend/src/pages/ProjectPage.js
+++ frontend/src/pages/ProjectPage.js
@@ -42,7 +42,7 @@ const ProjectPage = () => {
 
   useEffect(() => {
     axios.get(`${API.RESULTS}/${projectId}`).then((res) => {
-      dispatch(membersActions.loadMembers(res.data.memebers));
+      dispatch(membersActions.loadMembers(res.data.members));
       dispatch(resultsActions.loadResults(res.data.project_result));
     });
     axios.get(`${API.PAYS}/${projectId}`).then((res) => {
