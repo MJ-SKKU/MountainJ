1c1cb43e7afff11ce6f1c7853910b45ec8b6726a
Seran Kim <49436143+theran23@users.noreply.github.com>
2023-02-08 00:37:21 +0900

    fix: 결제내역 수정 - paymember 추가 및 추가한 paymember를 payer로 설정할 경우 에러 없도록

--- backend/api/views.py
+++ backend/api/views.py
@@ -422,8 +422,12 @@ class PayListAPI(APIView):
                 if payer.get('member_id') is not None:
                     payer = Member.objects.get(member_id=payer['member_id'])
                 else: #payer가 결제내역 탭에서 추된 참여자여서가 member_id가 없는 경우.
+                    print('hi')
                     username = payer.get('username')
-                    payer = Member.objects.create(username=username)
+                    print(username)
+                    payer = Member.objects.create(username=username,project=project)
+                    print(payer)
+
                     # paymember에도 있으 변경해줌.
                     for paymember in paymembers:
                         if paymember.get('member_id') is None and paymember.get('username') == username:
@@ -444,6 +448,7 @@ class PayListAPI(APIView):
                 pay = Pay.objects.create(project=project,payer=payer,title=title,money=money)
                 print('...')
                 #2. pay_member 생성
+                # todo: 리팩토링 필요.
                 for paymember in paymembers:
                     member = Member.objects.get(member_id=paymember['member_id'])
                     PayMember.objects.create(pay=pay,member=member)
@@ -487,7 +492,7 @@ class PayAPI(APIView):
                     payer = Member.objects.get(member_id=payer['member_id'])
                 else:  # payer가 결제내역 탭에서 추된 참여자여서가 member_id가 없는 경우.
                     username = payer.get('username')
-                    payer = Member.objects.create(username=username)
+                    payer = Member.objects.create(username=username, project=pay.project)
                     # paymember에도 있으 변경해줌.
                     for paymember in paymembers:
                         if paymember.get('member_id') is None and paymember.get('username') == username:
@@ -517,19 +522,19 @@ class PayAPI(APIView):
 
 
                 print(member_li)
+                #paymember 생성
                 id_li = []
                 for member in member_li:
                     id = member.get("member_id")
                     if id is None:
                         print(member.get("username"))
-                        # p = Project.objects.get(project_id=pay.project)
                         m = Member.objects.create(project=pay.project, username=member.get("username"))
                         pm = PayMember.objects.create(pay=pay, member_id=m.member_id)
                         print('.......')
                         id_li.append(pm.paymember_id)
                     else:
                         m = Member.objects.get(member_id=id)
-                        pm = PayMember.objects.get(pay=pay,member_id=m.member_id)
+                        pm = PayMember.objects.get_or_create(pay=pay, member_id=m.member_id)[0]
                         id_li.append(pm.paymember_id)
                 print('d')
                 db_id_li = list(PayMember.objects.filter(pay=pay).values_list('paymember_id',flat=True))
