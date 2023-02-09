ae92af9cecb3b3e84227c735515faacf02661169
Seran Kim <49436143+theran23@users.noreply.github.com>
2023-02-09 15:25:56 +0900

    feat: 개발환경별 HOST configuration 분리

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,8 @@
-// const HOST_URL =
-//   "http://ec2-43-201-71-106.ap-northeast-2.compute.amazonaws.com";
 
-const HOST_URL =
-  "http://mountainj.me";
-// const HOST_URL = "http://localhost";
+import HOST from "./hostConfig";
 
-
-const SERVER_URL = HOST_URL + ":8000/";
-// const CLIENT_URL = HOST_URL + ":3000/";
-const CLIENT_URL = HOST_URL;
+const CLIENT_URL = HOST.CLIENT_URL;
+const SERVER_URL = HOST.SERVER_URL;
 
 const REDIRECT_URI = CLIENT_URL + "/kakao/login";
 const REST_API_KEY = "43f9c4625042bd2d0d174ecf3708b12e";
