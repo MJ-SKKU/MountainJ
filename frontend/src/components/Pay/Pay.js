c4abe93013af039d3c38a9453ddcad8005c3eb8c
Suhyeon <fbtngus2789@naver.com>
2023-02-13 23:43:46 +0900

    fix: scrollbar로 인해 유저명 가려지는 에러 수정 중

--- frontend/src/components/Pay/Pay.js
+++ frontend/src/components/Pay/Pay.js
@@ -1,5 +1,5 @@
-import {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { FiChevronDown, FiEdit, FiTrash } from "react-icons/fi";
 import axios from "axios";
 
@@ -8,47 +8,35 @@ import UserProfile from "../UI/UserProfile";
 import PayEditModal from "../Modal/PayEditModal";
 import Modal from "../Modal/Modal";
 import Price from "../UI/Price";
-import {paysActions} from "../../store/Pays";
-
-import {projectActions} from "../../store/ProjectInfo";
-import {membersActions} from "../../store/Members";
+import { paysActions } from "../../store/Pays";
 
 const Pay = (props) => {
-
   const dispatch = useDispatch();
 
-
   const pay = props.pay;
-
 
   const [isAccordionFolded, setIsAccordionFolded] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-
   const [payer, setPayer] = useState({});
   const [payMembers, setPayMembers] = useState([]);
 
-
   useEffect(() => {
-
     const paymembersGetCall = async () => {
       try {
-        const res = await axios.get(`${API.PAYMEMBERS}/${pay.pay_id}`);// const data = res.data;
+        const res = await axios.get(`${API.PAYMEMBERS}/${pay.pay_id}`); // const data = res.data;
         setPayMembers(res.data);
-      } catch {
-      }
+      } catch {}
     };
     const payerGetCall = async () => {
       try {
-        const res = await axios.get(`${API.MEMBER}/${pay.payer}`);// const data = res.data;
+        const res = await axios.get(`${API.MEMBER}/${pay.payer}`); // const data = res.data;
         setPayer(res.data);
-      } catch {
-      }
+      } catch {}
     };
     paymembersGetCall();
     payerGetCall();
-  }, [props.pay]);
-
+  }, [pay]);
 
   const onAccordionIconClick = () => {
     setIsAccordionFolded((prevState) => {
@@ -57,14 +45,13 @@ const Pay = (props) => {
   };
 
   const onPayDelete = async (e) => {
-
     const title = e.currentTarget.title;
-    if(window.confirm(`"${title}"을 삭제하시겠습니까?`)){
+    if (window.confirm(`"${title}"을 삭제하시겠습니까?`)) {
       const res = await axios.delete(`${API.PAY}/${pay.pay_id}`);
-      if(res.status==200){
+      if (res.status === 200) {
         console.log("결제내역 삭제 성공");
         dispatch(paysActions.needUpdate());
-      }else{
+      } else {
         console.log("결제내역 삭제 실패");
       }
     }
@@ -96,30 +83,40 @@ const Pay = (props) => {
       </div>
       <div
         className={`${
-          props.userMember != null ? (
-              isAccordionFolded ? "h-0" : "h-32"
-          ) : (
-              isAccordionFolded ? "h-0" : null
-          )
+          props.userMember !== null
+            ? isAccordionFolded
+              ? "h-0"
+              : "h-32"
+            : isAccordionFolded
+            ? "h-0"
+            : null
         } transition-all duration-300 overflow-y-hidden`}
       >
         <div className="flex flex-col justify-center mx-auto -mt-1 w-11/12 bg-white shadow rounded-md">
           <div className="gap-3 flex justify-evenly w-full mx-auto items-center mt-5 px-5 pb-2.5 overflow-x-auto scrollbar-hide">
             {payMembers.map((member, idx) => {
-              return <UserProfile key={idx} user_id={member.user} username={member.username} />;
+              return (
+                <UserProfile
+                  key={idx}
+                  user_id={member.user}
+                  username={member.username}
+                />
+              );
             })}
           </div>
-          {
-            props.userMember != null && (
-                <div>
-                  <hr />
-                  <div className="flex justify-between px-4 py-2">
-                    <button onClick={onModalClick} ><FiEdit size="16" /></button>
-                    <button onClick={onPayDelete} title={props.pay.title} ><FiTrash size="16" /></button>
-                  </div>
-                </div>
-            )
-          }
+          {props.userMember != null && (
+            <div>
+              <hr />
+              <div className="flex justify-between px-4 py-2">
+                <button onClick={onModalClick}>
+                  <FiEdit size="16" />
+                </button>
+                <button onClick={onPayDelete} title={props.pay.title}>
+                  <FiTrash size="16" />
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
 
