bd692c7b949256d26e96ea89abdcabcec96a13a9
Suhyeon <fbtngus2789@naver.com>
2023-02-17 15:20:47 +0900

    fix: 사용방법 2번 오타 수정 및 pay 수정 시 사용방법 배너가 모달 상단 가리는 에러 해결

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,5 +1,7 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { FiX } from "react-icons/fi";
 
 import LandingPage from "./pages/LandingPage";
 import KakaoLogInPage from "./pages/KakaoLogInPage";
@@ -7,10 +9,6 @@ import UserPage from "./pages/UserPage";
 import ProjectPage from "./pages/ProjectPage";
 import Header from "./components/UI/Header";
 import Footer from "./components/UI/Footer";
-import Project from "./components/Project/Project";
-import { useDispatch, useSelector } from "react-redux";
-import { FiCheck, FiX } from "react-icons/fi";
-import { payActions } from "./store/PayInfo";
 import { pageStatusActions } from "./store/PageStatus";
 
 const Layout = () => {
@@ -25,32 +23,25 @@ const Layout = () => {
     dispatch(pageStatusActions.tutOpen(false));
   };
   const toForm = () => {
-    window.location="https://forms.gle/f4M4gatsZNfuBAnz6";
+    window.location = "https://forms.gle/f4M4gatsZNfuBAnz6";
   };
-  useEffect(() => {
-    console.log("hihihi");
-  }, [tutOpen]);
+
   useEffect(() => {
     dispatch(pageStatusActions.tutOpen(true));
-  }, []);
+  }, [dispatch]);
+
   return (
     <Fragment>
       <Header />
       {/*header 크기 만큼 */}
-      <div style={{ height: `56px` }}></div>
+      <div className="h-[56px]"></div>
       {/*이벤트 banner*/}
       {!isModalsOpen && (
         <div>
-          <div
-            className="fixed right-0 left-0 w-full   shadow z-50"
-            style={{ top: `46px`, fontSize: "0.65em" }}
-          >
+          <div className="fixed right-0 left-0 w-full shadow text-[0.65em] top-56px">
             {" "}
             {tutOpen && (
-              <div
-                className="flex justify-between items-center  shadow px-4 bg-lime"
-                style={{ height: `80px`, fontSize: "1em" }}
-              >
+              <div className="flex justify-between items-center shadow px-4 bg-lime h-[80px] text-[1em]">
                 <div className="text-center">
                   <b>
                     [마운틴제이] <br /> 사용방법
@@ -58,7 +49,7 @@ const Layout = () => {
                 </div>
                 <div className="flex mr-2">
                   1. 정산을 생성하세요. <br />
-                  2. [결제내역] 탭에서을 결제내역을 추가하세요. <br />
+                  2. [결제내역] 탭에서 결제내역을 추가하세요. <br />
                   3. 하단에 [공유하기] 버튼을 눌러 친구들과 함께해보세요. <br />
                   4. [정산결과] 탭에서 최소 송금횟수로 정산해보세요!
                 </div>
@@ -69,28 +60,28 @@ const Layout = () => {
             )}
             {tutOpen ? (
               <div
-                className="bg-white text-center pt-3"
-                style={{ height: `60px`, fontSize: "1rem", backgroundColor:`#E4E5E7` }}
+                className="bg-white text-center pt-3 h-[60px] text-[1rem] bg-[#E4E5E7]"
                 onClick={toForm}
               >
                 <p>
                   <b>설문조사</b>하고, <b>이벤트 참여</b>하러가기! 💸
                 </p>
 
-                <p style={{fontSize:`0.65em`}}>스타벅스 아메리카노 / 올리브영 1만원권</p>
-
+                <p style={{ fontSize: `0.65em` }}>
+                  스타벅스 아메리카노 / 올리브영 1만원권
+                </p>
               </div>
             ) : (
               <div
-                className="bg-white text-center"
-                style={{ height: `100px`, fontSize: "18px",paddingTop:`25px`,backgroundColor:`#D0DA59` }}
+                className="bg-white text-center h-[100px] text-lg pt-[25px] bg-[#D0DA59]"
                 onClick={toForm}
               >
                 <b>설문조사</b>하고, <b>이벤트 참여</b>하러가기! 💸
-                <br/>
-                <p style={{fontSize:`0.65em`}}>정산을 공유하고 설문에 참여하면 스타벅스 아메리카노 쿠폰을,
+                <br />
+                <p className="text-[0.65em]">
+                  정산을 공유하고 설문에 참여하면 스타벅스 아메리카노 쿠폰을,
                 </p>
-                <p style={{fontSize:`0.65em`}}>
+                <p className="text-[0.65em]">
                   설문을 열심히 작성해주신 20분에게는 올리브영 상품권을 드려요!
                 </p>
               </div>
@@ -99,9 +90,9 @@ const Layout = () => {
 
           {/*banner 크기만큼*/}
           {tutOpen ? (
-            <div style={{ height: `140px` }}></div>
+            <div className="h-[140px]"></div>
           ) : (
-            <div style={{ height: `100px` }}></div>
+            <div className="h-[100px]"></div>
           )}
         </div>
       )}
